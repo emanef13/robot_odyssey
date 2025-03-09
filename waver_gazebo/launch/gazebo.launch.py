793b647f53c99c1490964032ac58adb5f8fe821f
Gerasimos Maltezos <germa22091996@gmail.com>
2025-03-09 19:12:26 +0100

    Working implementation (with drift) for nav2 stack.

--- waver_gazebo/launch/gazebo.launch.py
+++ waver_gazebo/launch/gazebo.launch.py
@@ -1,23 +1,22 @@
 import os
 from pathlib import Path
+import xacro
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, RegisterEventHandler, SetEnvironmentVariable, ExecuteProcess
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable, ExecuteProcess
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration, Command, PathJoinSubstitution, FindExecutable
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
-from launch.event_handlers import OnProcessExit
-import xacro
 
 def generate_launch_description():
-    
+    # Paths
     waver_robot_description_path = os.path.join(
         get_package_share_directory('waver_description'))
-    
+        
     waver_robot_sim_path = os.path.join(
         get_package_share_directory('waver_gazebo'))
-    
+        
     # Set gazebo sim resource path
     gazebo_resource_path = SetEnvironmentVariable(
         name='GZ_SIM_RESOURCE_PATH',
@@ -26,13 +25,23 @@ def generate_launch_description():
             str(Path(waver_robot_description_path).parent.resolve())
             ]
         )
+    
+    rviz_config_file = os.path.join(waver_robot_description_path, 'config', 'nav.rviz')
 
+    # Path to robot description URDF
+    xacro_file = os.path.join(waver_robot_description_path,
+                              'urdf',
+                              'waver.xacro')
+    doc = xacro.process_file(xacro_file, mappings={'use_sim' : 'true'})
+    robot_desc = doc.toprettyxml(indent='  ')
+    params = {'robot_description': robot_desc}
+    
+    # Arguments
     paused_arg = DeclareLaunchArgument('paused', default_value='true')
     use_sim_time_arg = DeclareLaunchArgument('use_sim_time', default_value='true')
     gui_arg = DeclareLaunchArgument('gui', default_value='true')
     headless_arg = DeclareLaunchArgument('headless', default_value='false')
     debug_arg = DeclareLaunchArgument('debug', default_value='false')
-
     world_arg = DeclareLaunchArgument(
         'world_name',
         default_value='empty',  # ✅ Use Gazebo's built-in empty world
@@ -53,15 +62,7 @@ def generate_launch_description():
                 ]
              )
 
-    xacro_file = os.path.join(waver_robot_description_path,
-                              'urdf',
-                              'waver.xacro')
-    
-    doc = xacro.process_file(xacro_file, mappings={'use_sim' : 'true'})
-
-    robot_desc = doc.toprettyxml(indent='  ')
-
-    params = {'robot_description': robot_desc}
+    ## Initializing all the nodes ## 
 
     # (Optional) publish the robot description on /robot_description for TF, etc.
     # This is the typical approach in ROS 2: run robot_state_publisher
@@ -71,13 +72,23 @@ def generate_launch_description():
         output='screen',
         parameters=[params]
     )
-    # Sprawn the robot in Gazebo by reading from /robot_description
+
+    # EKF node
+    robot_localization_node = Node(
+        package='robot_localization',
+        executable='ekf_node',
+        name='ekf_node',
+        output='screen',
+        parameters=[os.path.join(waver_robot_sim_path, 'config/ekf.yaml'), {'use_sim_time': LaunchConfiguration('use_sim_time')}]
+    )
+
+    # Spawn the robot in Gazebo by reading from /robot_description
     spawn_robot = Node(
         package='ros_gz_sim',
         executable='create',
         output='screen',
         arguments=['-string', robot_desc,
-                   '-x', '0.0',
+                   '-x', '5.0',
                    '-y', '0.0',
                    '-z', '0.07',
                    '-R', '0.0',
@@ -87,29 +98,25 @@ def generate_launch_description():
                    '-allow_renaming', 'false'],
     )
 
-    load_joint_state_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'joint_state_broadcaster'],
-        output='screen'
-    )
-
-    load_forward_velocity_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'forward_velocity_controller'],
-        output='screen'
-    )
-
     # Bridge
     bridge = Node(
         package='ros_gz_bridge',
         executable='parameter_bridge',
-        arguments=['/scan@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan', "/imu@sensor_msgs/msg/Imu@gz.msgs.IMU", '/camera/image_raw@sensor_msgs/msg/Image@gz.msgs.Image',
-                   '/camera/camera_info@sensor_msgs/msg/CameraInfo@gz.msgs.CameraInfo'], # "/clock@rosgraph_msgs/msg/Clock@gz.msgs.Clock"
-        output='screen'
+        arguments=['/scan@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan', 
+                   "/imu@sensor_msgs/msg/Imu@gz.msgs.IMU", 
+                   "/odom@nav_msgs/msg/Odometry@gz.msgs.Odometry",
+                   '/camera/image_raw@sensor_msgs/msg/Image@gz.msgs.Image',
+                   '/camera/camera_info@sensor_msgs/msg/CameraInfo@gz.msgs.CameraInfo', 
+                   "/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock",
+                   "/joint_states@sensor_msgs/msg/JointState@gz.msgs.Model",
+                   "/tf@tf2_msgs/msg/TFMessage@gz.msgs.Pose_V",
+                   "/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist"],
+        output='screen',
+        parameters=[
+            {'use_sim_time': LaunchConfiguration('use_sim_time')}
+        ]
     )
-
-    rviz_config_file = os.path.join(waver_robot_description_path, 'config', 'waver_robot_config.rviz')
-
+    #  Launching RVIZ with navigation configuration
     rviz = Node(
         package="rviz2",
         executable="rviz2",
@@ -120,18 +127,6 @@ def generate_launch_description():
 
     # Build up the entire launch description
     return LaunchDescription([
-        RegisterEventHandler(
-            event_handler=OnProcessExit(
-                target_action=spawn_robot,
-                on_exit=[load_joint_state_controller],
-            )
-        ),
-        RegisterEventHandler(
-            event_handler=OnProcessExit(
-               target_action=load_joint_state_controller,
-               on_exit=[load_forward_velocity_controller],
-            )
-        ),
         gazebo_resource_path,
         world_arg,
         paused_arg,
@@ -143,5 +138,6 @@ def generate_launch_description():
         robot_state_publisher,
         spawn_robot,
         bridge,
+        robot_localization_node,
         rviz,
     ])
